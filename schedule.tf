bfdbbe45a90b619a4a7833878ff027e11687a512
Jeremiah Norris <jjno91@gmail.com>
2024-05-03 16:02:34 -0500

    add resource indices

--- schedule.tf
+++ schedule.tf
@@ -6,15 +6,15 @@ resource "aws_cloudwatch_event_rule" "this" {
 
 resource "aws_cloudwatch_event_target" "this" {
   count = 0
-  rule  = aws_cloudwatch_event_rule.this.name
-  arn   = aws_lambda_function.this.arn
+  rule  = aws_cloudwatch_event_rule.this[0].name
+  arn   = aws_lambda_function.this[0].arn
 }
 
 resource "aws_lambda_permission" "this" {
   count         = 0
   statement_id  = "AllowExecutionFromCloudWatch"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.this.function_name
+  function_name = aws_lambda_function.this[0].function_name
   principal     = "events.amazonaws.com"
-  source_arn    = aws_cloudwatch_event_rule.this.arn
+  source_arn    = aws_cloudwatch_event_rule.this[0].arn
 }
