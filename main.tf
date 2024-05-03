9b0db94f8a8165df9568cbf588e19ac5ab0a4d4d
Jeremiah Norris <jjno91@gmail.com>
2024-05-03 15:10:23 -0500

    load initial example code

---
+++ main.tf
@@ -0,0 +1,16 @@
+resource "aws_lambda_function" "this" {
+  count            = 1
+  function_name    = var.name
+  filename         = data.archive_file.this[0].output_path
+  handler          = "handler"
+  runtime          = "nodejs18.x"
+  source_code_hash = data.archive_file.this[0].output_base64sha256
+  role             = aws_iam_role.this[0].arn
+}
+
+data "archive_file" "this" {
+  count = 1
+  type        = "zip"
+  source_file = "test.js"
+  output_path = "this.zip"
+}
