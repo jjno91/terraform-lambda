4693a4824db6feae271de0d4db9fb41883aa3711
Jeremiah Norris <jjno91@gmail.com>
2024-05-18 20:58:07 -0500

    copy all to docker

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM public.ecr.aws/lambda/nodejs:20
 
 # Copy function code
-COPY main.js ${LAMBDA_TASK_ROOT}
+COPY . ${LAMBDA_TASK_ROOT}
--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 exports.handler = async (event) => {
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from main!'),
+        body: JSON.stringify('Hello from test!'),
     };
     return response;
 };
