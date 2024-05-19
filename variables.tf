6df69ffd54a3e24338d9406de6e161c7eb84b9db
Jeremiah Norris <jjno91@gmail.com>
2024-05-18 19:33:11 -0500

    add build steps

--- variables.tf
+++ variables.tf
@@ -8,3 +8,7 @@ variable "schedule_expression" {
   default     = null
   description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/cloudwatch_event_rule#schedule_expression"
 }
+
+variable "image" {
+  type = string
+}
